90aaa067ced1f597edc8ba8cddd37811d61f6740
espace2026 <noureddinerjili23@gmail.com>
2026-05-23 23:43:42 +0200

    Keep container alive

--- App.java
+++ App.java
@@ -1,5 +1,8 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello, World!");
+        while (true) {
+            Thread.sleep(3600000);
+        }
     }
 }
